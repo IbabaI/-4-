b47e9e60dfd580d143727929ba3b83a69693abae
user <2001mist@naver.com>
2021-09-29 10:45:57 +0900

    a

--- ch05/src/ch03_array/Ex10_1.java
+++ ch05/src/ch03_array/Ex10_1.java
@@ -30,6 +30,14 @@ public class Ex10_1 {
 				for(int s:intArr2)
 					System.out.print(s+" ");
 				System.out.println();
+				
+				//배열내의 특정 인덱스번호에 새로운 값을 저장
+				intArr2[3]=10;
+				for(int i:intArr1)
+				System.out.print(i+" ");
+				System.out.println();
+				for(int j:intArr2)
+				System.out.print(j+" ");
 		
 		
 
