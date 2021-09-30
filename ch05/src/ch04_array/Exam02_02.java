003ef0d7ab126f6ba86765d1fb09b6486306ab39
user <2001mist@naver.com>
2021-09-30 10:07:49 +0900

    z

---
+++ ch05/src/ch04_array/Exam02_02.java
@@ -0,0 +1,19 @@
+package ch04_array;
+
+import java.sql.SQLOutput;
+
+public class Exam02_02 {
+
+	public static void main(String[] args) {
+		
+		int	cnt = 0;
+		while(true) {
+			if(cnt==5) break;
+			System.out.println("hello");
+			System.out.println(++cnt+"È¸ ¿À·ù");
+		}
+		
+	}
+	
+
+}
