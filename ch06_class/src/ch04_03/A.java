2e46fe7599ee6d255ae155ac71e6620bf1b13baa
user <2001mist@naver.com>
2021-10-01 12:16:38 +0900

    xc

---
+++ ch06_class/src/ch04_03/A.java
@@ -0,0 +1,10 @@
+package ch04_03;
+
+//소스파일명과 public으로 선언된 class의 이름은 같아야 한다.
+ class A {
+
+}
+ public class B{}  //The public type B must be defined in its own file 
+ // 소스파일명과 다른 클래스에 public으로 선언하면 오류 발생
+ 
+class C{}
